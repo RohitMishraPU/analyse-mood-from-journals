03c020144cfcd55e645a46cbc02f546172815526
RohitMishraPU <rohitrklpny@gmail.com>
2024-07-07 18:18:09 +0530

    Dashboard layout added

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from "@clerk/nextjs";
+import { ReactNode } from "react"
+
+const DashboardLayout = ({children} : {children : ReactNode}) => {
+    return <div className="h-screen  w-screen relative flex">
+        <aside className=" basis-1/6 h-full border-r bg-slate-100 drop-shadow-sm w-full">
+            <div className="px-4 my-4">
+            <span className="text-3xl">JOURNALS</span>
+            </div>
+        </aside>
+        <section className="basis-5/6 h-full bg-slate-100 w-full">
+            <header className="py-4 border-b rounded-b-sm drop-shadow-sm  ">
+            <nav className="px-4 h-full">
+            <div className="flex items-center justify-end h-full">
+              <UserButton />
+            </div>
+          </nav>
+            </header>
+            {children}
+        </section>
+    </div>
+}
+
+export default DashboardLayout;
--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,35 @@
-const NewUser = () => {
-    return <>Hello</>
-}
+import { prisma } from '@/utils/db';
+import { auth, currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) throw 'Something went wrong';
+
+    const match = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+
+    if(!match) {
+        const newUser = await prisma.user.create({
+            data : {
+                email : user.emailAddresses[0].emailAddress,
+                name : user.fullName,
+                clerkId : user.id
+            }
+        })
+    }
+
+    redirect("/journal");
+  } catch (error) {}
+};
+const NewUser = async () => {
+   await createNewUser();
+  return <>Hello</>;
+};
 
 export default NewUser;
